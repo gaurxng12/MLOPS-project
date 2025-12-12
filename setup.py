8e496a8c9fd50ed67f4caaedaf8e4f3e3f6b8a3e
Gaurang Bhogle <bgaurangan@gmail.com>
2025-12-12 17:01:59 +0530

    project structure ready, virtual environment set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurang Bhogle",
+    author_email="bgaurangan@gmail.com",
+    packages=find_packages()
+)
